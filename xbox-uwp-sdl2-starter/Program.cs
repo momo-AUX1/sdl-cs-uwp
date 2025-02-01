64a7eeabbe83fdb088f9f19ef34169e14465216d
M A <xxmoonpower1234xx@gmail.com>
2025-02-01 21:14:06 +0100

    cube, controllers and third party controllers!

--- xbox-uwp-sdl2-starter/Program.cs
+++ xbox-uwp-sdl2-starter/Program.cs
@@ -6,14 +6,36 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using SDL2; 
+using System.Threading;
+using SDL2;
+using Windows.Gaming.Input;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.Foundation;
 
 namespace xbox_uwp_sdl2_starter
 {
     class Program
     {
-        [DllImport("extern_entry.dll", CallingConvention = CallingConvention.Cdecl)]
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ControllerInput
+        {
+            public float left_x;
+            public float left_y;
+            public float right_x;
+            public float right_y;
+            [MarshalAs(UnmanagedType.I1)]
+            public bool button_a;
+            [MarshalAs(UnmanagedType.I1)]
+            public bool button_b;
+        }
+
+        [DllImport("extern_entry_cube.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern int external_entry();
+
+        [DllImport("extern_entry_cube.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void update_controller_input(ControllerInput input);
 
         static void Main(string[] args)
         {
@@ -23,9 +45,92 @@ namespace xbox_uwp_sdl2_starter
             SDL.SDL_WinRTRunApp(mainFunction, IntPtr.Zero);
         }
 
+        private static ControllerInput PollThirdPartyController(CoreWindow window)
+        {
+            ControllerInput input = new ControllerInput();
+            VirtualKey leftUp = (VirtualKey)211;
+            VirtualKey leftDown = (VirtualKey)212;
+            VirtualKey leftLeft = (VirtualKey)214;
+            VirtualKey leftRight = (VirtualKey)213;
+
+            var stateUpL = window.GetKeyState(leftUp);
+            var stateDownL = window.GetKeyState(leftDown);
+            var stateLeftL = window.GetKeyState(leftLeft);
+            var stateRightL = window.GetKeyState(leftRight);
+
+            input.left_y = 0.0f;
+            if ((stateUpL & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.left_y = 1.0f;
+            if ((stateDownL & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.left_y = -1.0f;
+
+            input.left_x = 0.0f;
+            if ((stateLeftL & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.left_x = -1.0f;
+            if ((stateRightL & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.left_x = 1.0f;
+
+            VirtualKey rightUp = (VirtualKey)215;
+            VirtualKey rightDown = (VirtualKey)216;
+            VirtualKey rightLeft = (VirtualKey)218;
+            VirtualKey rightRight = (VirtualKey)217;
+
+            var stateUpR = window.GetKeyState(rightUp);
+            var stateDownR = window.GetKeyState(rightDown);
+            var stateLeftR = window.GetKeyState(rightLeft);
+            var stateRightR = window.GetKeyState(rightRight);
+
+            input.right_y = 0.0f;
+            if ((stateUpR & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.right_y = 1.0f;
+            if ((stateDownR & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.right_y = -1.0f;
+
+            input.right_x = 0.0f;
+            if ((stateLeftR & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.right_x = -1.0f;
+            if ((stateRightR & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down)
+                input.right_x = 1.0f;
+
+            VirtualKey buttonA = (VirtualKey)195;
+            VirtualKey buttonB = (VirtualKey)196;
+
+            var stateA = window.GetKeyState(buttonA);
+            var stateB = window.GetKeyState(buttonB);
+
+            input.button_a = (stateA & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down;
+            input.button_b = (stateB & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down;
+
+            return input;
+        }
+
+        private static ControllerInput GetInputFromUI(CoreWindow window)
+        {
+            ControllerInput result = new ControllerInput();
+            var op = window.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                result = PollThirdPartyController(window);
+            });
+            while (op.Status == AsyncStatus.Started)
+            {
+                Thread.Sleep(1);
+            }
+            return result;
+        }
+
+        private static void ControllerPollingLoop(CoreWindow window)
+        {
+            while (true)
+            {
+                ControllerInput input = GetInputFromUI(window);
+                update_controller_input(input);
+                Thread.Sleep(16); 
+            }
+        }
+
         private static int SDLMain(int argc, IntPtr argv)
         {
-            if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO) != 0)
+            if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_AUDIO | SDL.SDL_INIT_GAMECONTROLLER | SDL.SDL_INIT_JOYSTICK) != 0)
             {
                 Debug.WriteLine($"SDL Initialization failed: {SDL.SDL_GetError()}");
                 return -1;
@@ -67,6 +172,41 @@ namespace xbox_uwp_sdl2_starter
                 Debug.WriteLine($"Warning: Unable to set VSync: {SDL.SDL_GetError()}");
             }
 
+
+
+            var inputThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    var gamepads = Gamepad.Gamepads;
+                    if (gamepads.Count > 0)
+                    {
+                        var gamepad = gamepads[0];
+                        var reading = gamepad.GetCurrentReading();
+
+                        ControllerInput input = new ControllerInput
+                        {
+                            left_x = (float)reading.LeftThumbstickX,
+                            left_y = (float)reading.LeftThumbstickY,
+                            right_x = (float)reading.RightThumbstickX,
+                            right_y = (float)reading.RightThumbstickY,
+                            button_a = (reading.Buttons & GamepadButtons.A) != 0,
+                            button_b = (reading.Buttons & GamepadButtons.B) != 0
+                        };
+
+                        update_controller_input(input);
+                    }
+                    Thread.Sleep(16); 
+                }
+            });
+            inputThread.IsBackground = true;
+            inputThread.Start();
+
+
+            CoreWindow coreWindow = CoreWindow.GetForCurrentThread();
+            var thirdinputThread = new Thread(() => ControllerPollingLoop(coreWindow));
+            thirdinputThread.IsBackground = true;
+            thirdinputThread.Start();
 
             int ret = external_entry();
             Debug.WriteLine($"external_entry returned: {ret}");
